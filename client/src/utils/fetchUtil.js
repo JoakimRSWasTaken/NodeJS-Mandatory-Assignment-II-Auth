d453b42f012299737dbe13fddbbe7042242c851f
Joakim Rosdahl Severinsen <jose0001@stud.ek.dk>
2026-04-19 18:39:33 +0200

    Tilføjet en fetchUtil.js som har fetches. De minder om dem fra undervisningen, men jeg har tilføjet en try-catch til fetchPost

---
+++ client/src/utils/fetchUtil.js
@@ -0,0 +1,28 @@
+// @ts-nocheck so we do not get type checks as if this was TypeScript
+
+export async function fetchGet(endpoint) {
+    try {
+        const response = await fetch(`${import.meta.env.VITE_BASE_URL}${endpoint}`, {
+            credentials: 'include'
+        });
+        return await response.json();
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export async function fetchPost(endpoint, body) {
+    try {
+        const response = await fetch(`${import.meta.env.VITE_BASE_URL}${endpoint}`, {
+            method: "POST",
+            credentials: 'include',
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(body)
+        });
+        return await response.json();
+    } catch (error) {
+        console.log(error);
+    }
+}
